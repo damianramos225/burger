baac8464a99da28911f3840af7fcd41195cded11
Damian Ramos <damianramos225@gmail.com>
2020-05-14 14:22:00 -0400

    last minute changes again

--- config/orm.js
+++ config/orm.js
@@ -12,8 +12,18 @@ function translatesql(obj) {
     var arr = [];
     for (var key in ob) {
         var value = ob[key];
-        if (Object.hasOwn)
+        if (Object.hasOwnProperty.call(ob, key)) {
+
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+
+            arr.push(key + "=" + value);
+        }
     }
+
+
+    return arr.toString();
 }
 
 
